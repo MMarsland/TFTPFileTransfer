3d578f69225582ca20cccc87aecdcb620ee67fe9
scott <scott@DESKTOP-87PQQAS>
2019-05-13 11:53:32 -0400

    Incomplete client class

--- TFTP File Transfer Project/src/Client.java
+++ TFTP File Transfer Project/src/Client.java
@@ -26,17 +26,19 @@ public class Client extends Thread {
 	
 	public void run()
 	{
-		byte data[] = new byte[516];
 		byte sendData[] = new byte[516];
+		byte data[] = new byte[516];
 		DatagramPacket receivePacket = new DatagramPacket(data, data.length);
 		boolean transfer = false;
 		
+		String readTo, writeFrom;  //The client-side file location that we'll transfer data to/from
+		
 		Scanner in = new Scanner(System.in);  //Scanner for inputting commands
-		String command;
+		String command, msg;
 		String[] split;
 		
 		while(!transfer) {  //While not transferring, scan for and handle commands
-			System.out.println("Enter a command in the format: requestType sourceFile destinationFile");
+			System.out.println("Enter a command in the format: requestType sourceFilePath destinationFilePath");
 			System.out.println("Enter 'shutdown' to close client.");
 			command = in.nextLine();
 			split = command.split("\\s+");
@@ -47,15 +49,24 @@ public class Client extends Thread {
 				while(this.isAlive()) {}
 				System.out.println("Shutting down.");
 				System.exit(0);
+			} else if(split[0].toLowerCase().equals("read") || split[0].toLowerCase().equals("r")) {
+				sendData[0] = (byte)0x0;
+				sendData[1] = (byte)0x1;
+				readTo = split[1];
+			} else if(split[0].toLowerCase().equals("write") || split[0].toLowerCase().equals("w")) {
+				sendData[0] = (byte)0x0;
+				sendData[1] = (byte)0x2;
+				writeFrom = split[1];
 			}
 			
 		}
 		
-		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
 		
+		//sendpacket for later in the code
+		//DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
 		
 	}
-	
+
 	public static void main(String[] args) {
 		
 		System.out.println("Client running");
